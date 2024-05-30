915fd15c60e8626971fd2561d0da7d8ce0dde9cf
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-05-30 10:57:31 +0200

    correction exo indexeurs

---
+++ Exo_Banque_05_Indexeurs/Model/Banque.cs
@@ -0,0 +1,38 @@
+﻿using Exo_Banque_04_Proprietes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Banque_05_Indexeurs.Model
+{
+    internal class Banque
+    {
+        public string Nom { get; set; }
+        private readonly Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+        
+        public Courant this[string key]
+        {
+            get
+            {
+                return _comptes[key];
+            }
+        }
+
+        public void Ajouter(Courant courant)
+        {
+            if( !_comptes.ContainsKey(courant.Numero) )
+            {
+                _comptes.Add(courant.Numero, courant);
+            }
+        }
+
+        public void Supprimer(string numero)
+        {
+            _comptes.Remove(numero);
+        }
+
+    }
+}
---
+++ Exo_Banque_05_Indexeurs/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exo_Banque_04_Proprietes.Model;
+using Exo_Banque_05_Indexeurs.Model;
+
+Banque banque = new Banque() { Nom= "ma banque" };
+
+Personne Amandine = new Personne { Nom = "Lottelin", Prenom = "Amandine", DateNaissance = DateTime.Now };
+Courant courant = new Courant() { Numero = "1", Titulaire=Amandine };
+
+banque.Ajouter(courant);
+
+Courant courant1 = banque[Console.ReadLine()];
