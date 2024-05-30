7a033f6f450464681c408dca0254860236967b0c
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-05-30 14:20:36 +0200

    correction exo surcharge ope

---
+++ Exo_Banque_06_SurchargeOperateur/Model/Banque.cs
@@ -0,0 +1,54 @@
+﻿using Exo_Banque_06_Proprietes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Banque_05_Indexeurs.Model
+{
+    internal class Banque
+    {
+        public string Nom { get; set; }
+        private readonly Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+        
+        public Courant this[string key]
+        {
+            get
+            {
+                return _comptes[key];
+            }
+        }
+
+        public void Ajouter(Courant courant)
+        {
+            if( !_comptes.ContainsKey(courant.Numero) )
+            {
+                _comptes.Add(courant.Numero, courant);
+            }
+        }
+
+        public void Supprimer(string numero)
+        {
+            _comptes.Remove(numero);
+        }
+
+
+        public double AvoirDesComptes(Personne titulaire)
+        {
+            double rslt = 0;
+
+            foreach( Courant compte in _comptes.Values )
+            {
+                if( compte.Titulaire.Equals(titulaire) )
+                {
+                    rslt += compte;
+                }
+            }
+
+            return rslt;
+        }
+
+    }
+}
---
+++ Exo_Banque_06_SurchargeOperateur/Model/Courant.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Exo_Banque_06_Proprietes.Model
+{
+    internal class Courant
+    {
+        private string _Numero;
+        private double _Solde;
+        private double _LigneDeCredit;
+        private Personne _Titulaire;
+
+        // Getters - Setters
+        public string Numero
+        {
+            get { return _Numero; }
+            set { _Numero = value; }
+        }
+
+        public double Solde
+        {
+            get { return _Solde; }
+            private set { _Solde = value; }
+        }
+
+        public double LigneDeCredit
+        {
+            get { return _LigneDeCredit; }
+            set
+            {
+                if (value < 0) return; // Nous viendrons remplacer cette ligne par une exception
+
+                _LigneDeCredit = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get { return _Titulaire; }
+            set { _Titulaire = value; }
+        }
+
+        public void Retrait(double Montant)
+        {
+            if (Montant <= 0) return; // Nous viendrons remplacer cette ligne par une exception
+
+            if (Solde - Montant < LigneDeCredit) return;
+
+            Solde -= Montant;
+        }
+
+        public void Depot(double Montant)
+        {
+            if (Montant <= 0) return; // Nous viendrons remplacer cette ligne par une exception
+
+            Solde += Montant;
+        }
+
+
+        public static double operator +( Courant c1, Courant c2 )
+        {
+            double s1 = c1._Solde < 0 ? 0 : c1._Solde;
+            double s2 = c2._Solde < 0 ? 0 : c2._Solde;
+
+            return s1 + s2;
+        }
+        public static double operator +(double solde, Courant c2)
+        {
+            double s2 = c2._Solde < 0 ? 0 : c2._Solde;
+
+            return solde + s2;
+        }
+    }
+}
